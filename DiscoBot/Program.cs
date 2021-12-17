f96783505ab56498d337904e2b148726a0b8fffb
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2021-12-17 20:34:43 +0000

    Setup project

---
+++ DiscoBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscoBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
