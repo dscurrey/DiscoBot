4ddfdc670c2f792ae2e187be0e044d5105ec8e66
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2021-12-17 23:29:20 +0000

    Implemented naïve, rudimentary way of creating and sending images

---
+++ DiscoBot/CommandModules/DiscoImageModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DiscoBot.Utils;
+using Discord.Commands;
+
+#pragma warning disable CA1416
+
+namespace DiscoBot.CommandModules
+{
+    public class DiscoImageModule : ModuleBase
+    {
+        [Command("test")]
+        public async Task TestDrawCommand([Remainder] string args = null)
+        {
+            await Context.Channel.TriggerTypingAsync();
+
+            Image image;
+            if (args == null)
+                image = ImageUtils.DrawSimpleDisco("TEST");
+            else
+                image = ImageUtils.DrawSimpleDisco(args);
+
+            var filename = Guid.NewGuid() + ".jpg";
+            image.Save(filename);
+
+            await Context.Channel.SendFileAsync(filename);
+
+            File.Delete(filename);
+        }
+    }
+}
+
+#pragma warning restore CA1416
---
+++ DiscoBot/Utils/ImageUtils.cs
@@ -0,0 +1,48 @@
+﻿using System.Drawing;
+using System.Drawing.Text;
+
+#pragma warning disable CA1416
+
+
+namespace DiscoBot.Utils
+{
+    public class ImageUtils
+    {
+        private static PrivateFontCollection LoadFonts()
+        {
+            var collection = new PrivateFontCollection();
+            collection.AddFontFile(@"resources\LibreBaskerville-Regular.ttf");
+            collection.AddFontFile(@"resources\LibreBaskerville-Bold.ttf");
+            collection.AddFontFile(@"resources\LibreBaskerville-Italic.ttf");
+            return collection;
+        }
+
+        public static Image DrawSimpleDisco(string text)
+        {
+            var fontCollection = LoadFonts();
+            var img = new Bitmap(1, 1);
+            var drawing = Graphics.FromImage(img);
+
+            var font = new Font(new FontFamily("Libre Baskerville", fontCollection), 36);
+            var textSize = drawing.MeasureString(text, font);
+
+            img.Dispose();
+            drawing.Dispose();
+
+            img = new Bitmap((int)textSize.Width, (int)textSize.Height);
+            drawing = Graphics.FromImage(img);
+            drawing.Clear(Color.Gray);
+
+            var textBrush = new SolidBrush(Color.White);
+            drawing.DrawString(text, font, textBrush, 0, 0);
+            drawing.Save();
+
+            textBrush.Dispose();
+            drawing.Dispose();
+
+            return img;
+        }
+    }
+}
+
+#pragma warning restore CA1416
