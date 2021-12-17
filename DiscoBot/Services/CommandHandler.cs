ab308da28f9ad0685a262653fe2ea8495cab566b
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2021-12-17 21:40:29 +0000

    Added simple command handler

---
+++ DiscoBot/Services/CommandHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiscoBot.Services
+{
+    public class CommandHandler
+    {
+        private readonly IConfiguration _config;
+        private readonly CommandService _cmdServ;
+        private readonly DiscordSocketClient _client;
+        private readonly IServiceProvider _services;
+        private readonly char _prefix;
+
+        public CommandHandler(IServiceProvider services)
+        {
+            _services = services;
+            _config = _services.GetRequiredService<IConfiguration>();
+            _client = _services.GetRequiredService<DiscordSocketClient>();
+            _cmdServ = _services.GetRequiredService<CommandService>();
+
+            _prefix = char.Parse(_config["Discord:Prefix"]);
+
+            // Hook
+            _cmdServ.CommandExecuted += CommandExecutedAsync;
+            _client.MessageReceived += MessageReceivedAsync;
+        }
+
+        public async Task InitAsync() => await _cmdServ.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+
+        private async Task MessageReceivedAsync(SocketMessage rawMessage)
+        {
+            // Check if message is valid
+            if (rawMessage is not SocketUserMessage { Source: MessageSource.User } message)
+                return;
+            var argPos = 0;
+            if (!message.HasMentionPrefix(_client.CurrentUser, ref argPos) || message.HasCharPrefix
+                (_prefix, ref argPos))
+                return;
+
+            var context = new SocketCommandContext(_client, message);
+            // Execute Matching Commands
+            await _cmdServ.ExecuteAsync(context, argPos, _services);
+        }
+
+        private async Task CommandExecutedAsync(Optional<CommandInfo> command, ICommandContext context, IResult result)
+        {
+            if (!command.IsSpecified)
+            {
+                Console.WriteLine("Command failed to execute");
+                return;
+            }
+
+            if (result.IsSuccess)
+            {
+                Console.WriteLine("Command successful");
+                return;
+            }
+
+            await context.Channel.SendMessageAsync("Something went wrong.");
+        }
+    }
+}
